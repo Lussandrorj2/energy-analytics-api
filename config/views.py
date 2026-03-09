8dab886a712e2d66130014c621fef9a72f96fad7
Lussandro Farias <167385164+Lussandrorj2@users.noreply.github.com>
2026-03-09 14:31:48 -0300

    Atualizar o views.py com login e logout

--- config/views.py
+++ config/views.py
@@ -45,3 +45,37 @@ def health_check(request):
             "status": "error",
             "database": "disconnected"
         }, status=500)
+        
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+
+
+def login_view(request):
+
+    if request.method == "POST":
+
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+
+            login(request, user)
+
+            return redirect("/dashboard/")
+
+        else:
+
+            return render(request, "login.html", {
+                "erro": "Usuário ou senha inválidos"
+            })
+
+    return render(request, "login.html")
+    
+    
+def logout_view(request):
+
+    logout(request)
+
+    return redirect("/login/")
