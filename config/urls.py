3ab37450beb99848942937e70b9e1e04655fc35e
Lussandro Farias <167385164+Lussandrorj2@users.noreply.github.com>
2026-03-09 14:34:23 -0300

    Atualizar o urls.py add login and logout

--- config/urls.py
+++ config/urls.py
@@ -13,7 +13,7 @@ from django.http import JsonResponse
 from django.db import connection
 from django.db.utils import OperationalError
 from .views import home, health_check, dashboard, clientes_view, consumo, analytics_page,top_consumidores_page,anomalias_page
-
+from .views import login_view, logout_view
 
 router = DefaultRouter()
 router.register(r'clientes', ClienteViewSet)
@@ -25,17 +25,21 @@ urlpatterns = [
     path("dashboard/", dashboard, name="dashboard"),
     path("clientes-view/", clientes_view, name="clientes"),
     path("analytics/", analytics_page, name="analytics"),
+    path("consumo/", consumo, name="consumo"),
     path('admin/', admin.site.urls),
     path("health/", health_check, name="health"),
     path("top-consumidores/", top_consumidores_page, name="top_consumidores_page"),
     path("anomalias/", anomalias_page, name="anomalias_page"),
-
-    path('',include('apps.consumption.urls')),
-
+    path('', include('apps.consumption.urls')),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
+    
     # JWT
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
+    
+    
+    
     # Versionamento
     path('api/v1/', include([
         path('', include(router.urls)),
