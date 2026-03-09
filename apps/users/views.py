50bf540ff98f0a380f3599b0bb14d50875bc7031
Lussandro <lussandrorj2@hotmail.com>
2026-03-09 17:22:39 -0300

    fix: remove ClienteViewSet from users app and keep only authentication views

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,29 +1,19 @@
-from rest_framework import filters
-from rest_framework.viewsets import ModelViewSet
-from .models import Cliente
-from .serializers import ClienteSerializer
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
 
-class ClienteViewSet(ModelViewSet):
-
-    queryset = Cliente.objects.all()
-    serializer_class = ClienteSerializer
-
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['nome']
-
 def register(request):
 
     if request.method == "POST":
+
         form = UserCreationForm(request.POST)
 
         if form.is_valid():
             form.save()
-            return redirect("/")
+            return redirect("/login/")
 
     else:
+
         form = UserCreationForm()
 
     return render(request, "register.html", {"form": form})
