e1260dde4d9ab4aa5be303d2f4eacbc768018f9b
Lussandro <lussandrorj2@hotmail.com>
2026-03-09 19:48:01 -0300

    refactor(template): improve navigation using Django url tags and auth menu

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,14 +1,21 @@
-# apps/users/views.py
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.db import IntegrityError
+
 
 def register(request):
+
     if request.method == "POST":
         form = UserCreationForm(request.POST)
+
         if form.is_valid():
-            form.save()
-            return redirect("/login/")  # Or use reverse("login")
+            try:
+                form.save()
+                return redirect("login")
+            except IntegrityError:
+                form.add_error(None, "Erro ao criar usuário. Tente outro username.")
+
     else:
         form = UserCreationForm()
-    
+
     return render(request, "register.html", {"form": form})
