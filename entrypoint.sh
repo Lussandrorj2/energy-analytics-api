12b527f44538fa0edac3358df6c774c5d2a20d33
Lussandro Farias <167385164+Lussandrorj2@users.noreply.github.com>
2026-04-16 05:21:40 -0300

    Refactor entrypoint.sh to use environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,16 @@
-#!/bin/sh
-
-echo "Applying migrations..."
-python manage.py migrate --noinput
-
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
-echo "Creating superuser if not exists..."
-
 python manage.py shell <<EOF
+import os
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@email.com", "admin123")
+username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@email.com")
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "admin123")
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
     print("Superuser created")
 else:
     print("Superuser already exists")
 EOF
-
-echo "Starting server..."
-
-exec "$@"
